5a51ac4348d718b74eaa5ceef36b5d9d611db60f
Harry Ryder <hryder@electronrx.com>
2022-01-17 19:06:05 +0000

    Moved logic to API

--- lib/module/sponsors/api/sponsors_api_client.dart
+++ lib/module/sponsors/api/sponsors_api_client.dart
@@ -3,12 +3,14 @@ import 'dart:math';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
+import '../util/grid_generator.dart';
+import '../models/sponsor.dart';
+
 class SponsorsApiClient {
   static Random random = Random();
 
-  static Future<List> fetchSponsors(int page) async {
-    // final List<Sponsor> _loadedSponsors = [];
-    List _loadedData = [];
+  static Future<List<Sponsor>> fetchSponsors(int page) async {
+    List<Sponsor> _loadedSponsors = [];
 
     final String url =
         'https://61e2d5ee3050a100176822b4.mockapi.io/api/sponsors?page=$page&limit=3';
@@ -20,11 +22,31 @@ class SponsorsApiClient {
         throw Exception();
       }
 
-      _loadedData = jsonDecode(_response.body) as List;
+      final _data = jsonDecode(_response.body) as List;
+
+      for (var sponsor in _data) {
+        // Generate product image grids for each sponsor
+        final _grids = GridGenerator.generateGrid(sponsor['productImages']);
+        final _coverGrid = _grids['coverGrid'];
+        final _expandedGrids = _grids['expandedGrids'];
+        final bool _hasGrid = _coverGrid != null;
+
+        // Create new sponsor
+        final Sponsor _newSponsor = Sponsor(
+          id: random.nextInt(10000),
+          name: sponsor['companyName'],
+          logo: sponsor['logoImageNoPadding'],
+          images: sponsor['productImages'],
+          coverImageGrid: _coverGrid,
+          expandedImageGrids: _expandedGrids,
+          hasImageGrid: _hasGrid,
+        );
+        _loadedSponsors.add(_newSponsor);
+      }
     } catch (error) {
       rethrow;
     }
 
-    return _loadedData;
+    return _loadedSponsors;
   }
 }
--- lib/module/sponsors/repository/sponsors_repository.dart
+++ lib/module/sponsors/repository/sponsors_repository.dart
@@ -8,33 +8,8 @@ class SponsorsRepository {
   static Random random = Random();
 
   static Future<List<Sponsor>> getSponsors(int page) async {
-    final List<Sponsor> _loadedSponsors = [];
-
-    try {
-      final List _data = await SponsorsApiClient.fetchSponsors(page);
-
-      for (var sponsor in _data) {
-        // Generate product image grids for each sponsor
-        final _grids = GridGenerator.generateGrid(sponsor['productImages']);
-        final _coverGrid = _grids['coverGrid'];
-        final _expandedGrids = _grids['expandedGrids'];
-        final bool _hasGrid = _coverGrid != null;
-
-        // Create new sponsor
-        final Sponsor _newSponsor = Sponsor(
-          id: random.nextInt(10000),
-          name: sponsor['companyName'],
-          logo: sponsor['logoImageNoPadding'],
-          images: sponsor['productImages'],
-          coverImageGrid: _coverGrid,
-          expandedImageGrids: _expandedGrids,
-          hasImageGrid: _hasGrid,
-        );
-        _loadedSponsors.add(_newSponsor);
-      }
-    } catch (error) {
-      rethrow;
-    }
+    List<Sponsor> _loadedSponsors = [];
+    _loadedSponsors = await SponsorsApiClient.fetchSponsors(page);
     return _loadedSponsors;
   }
 }
